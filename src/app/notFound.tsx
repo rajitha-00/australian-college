47569a05aa506e1cfc07d81cc26805f9db1d76a0
rajitha-00 <rajithatw07102@gmail.com>
2024-10-13 17:13:03 +0530

    fixed

--- src/app/notFound.tsx
+++ src/app/notFound.tsx
@@ -19,8 +19,8 @@ export default function NotFound() {
           Page Not Found <br /> (Might be Still Development)
         </h2>
         <p className="mb-4 text-[16px] text-center font-light text-gray-500 dark:text-gray-400">
-          Sorry, we can't find that page. You'll find lots to explore on the
-          home page.
+          Sorry, we can&apos;t find that page. You&apos;ll find lots to explore
+          on the home page.
         </p>
         <Link href="/">
           <div
